4097aad759a7770ec54f4392981c71d495a19f41
Yiqiang Zeng <1136838669@qq.com>
2019-03-29 10:54:54 +0800

    0329

--- Package.swift
+++ Package.swift
@@ -14,14 +14,12 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", "3.0.0"..<"4.0.0"),
 		.package(url: "https://github.com/SwiftORM/Postgres-StORM.git", "3.0.0"..<"4.0.0"),
-		.package(url: "https://github.com/stormpath/Turnstile.git", from: "1.0.0"),
         .package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", from: "3.0.2")
 	],
 	targets: [
 		.target(name: "TurnstilePerfect", dependencies: [
 			"PerfectHTTPServer",		//HTTP服务
 			"PostgresStORM", 			//PostgreSQL ORM
-			"Turnstile",				//请求认证
 			"PerfectMustache"			//模板引擎
 			])
 	]
